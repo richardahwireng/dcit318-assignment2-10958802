1877ba6eab055c8b748e27f01e7dc596b26460b9
richardahwireng <roahwireng@st.ug.edu.gh>
2024-06-17 16:44:20 +0000

    fix the code errors in Abstract classes and methods

--- AbstractClassesandMethods/Program.cs
+++ AbstractClassesandMethods/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myAnimal.MakeSound();
+            myDog.MakeSound();
+            myCat.MakeSound();
+        }
+    }
+}
